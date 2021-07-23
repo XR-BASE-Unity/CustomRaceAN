667f755c31bfc74c2eef792a3d0dfdf7c0186b6e
AntonovAN <alexey-n-antonov@yandex.ru>
2021-07-24 00:49:32 +0300

    Init project
    Create PlayerController using WASD

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float angle;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 posOffset = gameObject.transform.position;
+        Vector3 rotOffset = Vector3.zero;
+      
+      if (Input.GetKey(KeyCode.W))
+      {
+            posOffset += new Vector3 (0f, 0f, Time.deltaTime * speed);
+      }
+      if (Input.GetKey(KeyCode.S))
+      {
+            posOffset -= new Vector3(0f, 0f, Time.deltaTime * speed);
+        }
+
+      if (Input.GetKey(KeyCode.A))
+      {
+            rotOffset = new Vector3(0f, -Time.deltaTime * angle, 0f);
+      }
+      if (Input.GetKey(KeyCode.D))
+      {
+            rotOffset = new Vector3(0f, Time.deltaTime * angle, 0f);
+      }
+
+        gameObject.transform.position = posOffset;
+        gameObject.transform.Rotate(rotOffset);
+    }
+}
